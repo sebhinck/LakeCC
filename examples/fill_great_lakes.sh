a3db8e88f8a530da12195e4aa37f2898f2fc3fbd
Sebastian Hinck <sebastian.hinck@awi.de>
2018-10-24 09:21:40 +0200

    Updated great lakes example

--- examples/fill_great_lakes.sh
+++ examples/fill_great_lakes.sh
@@ -4,4 +4,14 @@ thisdir=$(dirname $0)
 libdir=$(readlink -f ${thisdir}/..)
 
 export PYTHONPATH="$libdir:$PYTHONPATH"
-echo $PYTHONPATH
+
+EtopoFile="${thisdir}/ETOPO1_Bed_c_gmt4.nc"
+GreatLakesFile="${thisdir}/GreatLakes.nc"
+
+#get and convert Etopo1 dataset (if not present)
+${thisdir}/getEtopo.sh ${thisdir}
+
+#Crop GreatLakes [-93, -73, 40, 49]
+${thisdir}/CropAndFilterMap.py ${EtopoFile} ${GreatLakesFile} -93 -73 40 49
+
+
