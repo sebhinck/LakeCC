02a7212eb5a672a56a6361d326c5338e6d32bdea
Sebastian Hinck <sebastian.hinck@awi.de>
2018-10-24 09:20:02 +0200

    Deleted CropGreatLakes.py and made CropAndFilterMap.py executable

--- examples/CropAndFilterMap.py
+++ examples/CropAndFilterMap.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr  3 14:31:27 2017
-
-@author: sebastian
-"""
+#!/usr/bin/env python
 
 def getIndFromCoord(x, x_coord):
     
@@ -32,14 +27,14 @@ def CropAndFilterMap(fIn, fOut, lim, FilterN = 10, FilterMethod = 'square'):
     
     xV = ncIn.variables['x']
     yV = ncIn.variables['y']
-    topgV = ncIn.variables['z']
+    usurfV = ncIn.variables['z']
     
     xmin, xmax, ymin, ymax = getIndRangeFromCoord(lim[0], lim[1], lim[2], lim[3], xV[:], yV[:])
     xind = range(xmin, xmax + 1)
     yind = range(ymin, ymax + 1)    
     
     
-    topg = np.copy(topgV[yind, xind]).astype("double")
+    usurf = np.copy(usurfV[yind, xind]).astype("double")
    
     ncout = NC.Dataset(fOut, 'w')
     
@@ -52,15 +47,59 @@ def CropAndFilterMap(fIn, fOut, lim, FilterN = 10, FilterMethod = 'square'):
     yN = ncout.createVariable('y', 'f4', 'y')
     ncout.variables['y'][:] = yV[yind]
     
-    topgN = ncout.createVariable('topg' , 'f4', ['y', 'x'])
-    ncout.variables['topg'][:] = topg
+    usurfN = ncout.createVariable('usurf' , 'f4', ['y', 'x'])
+    ncout.variables['usurf'][:] = usurf
     
     if not (FilterMethod is None):
         if (FilterMethod == 'Gaussian'):
             import scipy.ndimage as ndi
-            filtered = ndi.gaussian_filter(topg, sigma = FilterN,  mode = 'nearest')
+            filtered = ndi.gaussian_filter(usurf, sigma = FilterN,  mode = 'nearest')
             
-            topgFilteredV = ncout.createVariable("topg_filtered", 'f4', ['y', 'x'])
-            ncout.variables['topg_filtered'][:] = filtered
+            usurfFilteredV = ncout.createVariable("usurf_filtered", 'f4', ['y', 'x'])
+            ncout.variables['usurf_filtered'][:] = filtered
     
     ncout.close()
+    
+    
+def main(argv):
+  
+  if len(sys.argv) > 1:
+    fIn = sys.argv[1]
+  else:
+    fIn = 'ETOPO1_Bed_c_gmt4.nc'
+
+  if len(sys.argv) > 2:
+    fOut = sys.argv[2]
+  else:
+    fOut = 'out.nc'
+
+  lim=[-180.0, 180.0, -90.0, 90.0]
+
+  if len(sys.argv) > 3:
+    lim[0] = float(sys.argv[3])
+  if len(sys.argv) > 4:
+    lim[1] = float(sys.argv[4])
+  if len(sys.argv) > 5:
+    lim[2] = float(sys.argv[5])
+  if len(sys.argv) > 6:
+    lim[3] = float(sys.argv[6])
+    
+  if len(sys.argv) > 7:
+    Filter = sys.argv[7]
+    if Filter == 'None':
+      Filter = None
+  else:
+    Filter = None
+    
+  if len(sys.argv) > 8:
+    FilterN = int(sys.argv[8])
+  else:
+    FilterN = 1
+    
+  CropAndFilterMap(fIn, fOut, lim, FilterN, Filter)
+
+
+if __name__ == "__main__":
+  import sys
+  main(sys.argv)
+  
--- examples/CropGreatLakes.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-from CropAndFilterMap import *
-
-CropAndFilterMap(fIn = './ETOPO1_Bed_c_gmt4.nc', fOut = "./GreatLakes.nc", lim = [-93, -73, 40, 49], FilterMethod = None)
