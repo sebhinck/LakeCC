7fc72a4f0053ffa19b8e6d5489b36660b0789e4e
Sebastian Hinck <sebastian.hinck@awi.de>
2018-10-24 19:18:16 +0200

    Modified example

--- examples/fill_great_lakes.sh
+++ examples/fill_great_lakes.sh
@@ -7,12 +7,13 @@ export PYTHONPATH="$libdir:$PYTHONPATH"
 
 EtopoFile="${thisdir}/ETOPO1_Bed_c_gmt4.nc"
 GreatLakesFile="${thisdir}/GreatLakes.nc"
+OutFile="${thisdir}/GreatLakes_filled.nc"
 
 #get and convert Etopo1 dataset (if not present)
 ${thisdir}/getEtopo.sh ${thisdir}
 
 #Crop GreatLakes [-93, -73, 40, 49]
-${thisdir}/CropAndFilterMap.py ${EtopoFile} ${GreatLakesFile} -93 -73 40 49 'Gaussian' 2
+${thisdir}/CropAndFilterMap.py ${EtopoFile} ${GreatLakesFile} -93 -73 40 49 'Gaussian' 3
 
 #Use the Python interface to fill the map
-${libdir}/FillLakes.py --sea-level 123 -i ${GreatLakesFile} -o test.nc -dz 1 -zMin -100 -zMax 800
+${libdir}/FillLakes.py --sea-level 123 -i ${GreatLakesFile} -o ${OutFile} -dz 10 -zMin -100 -zMax 800
